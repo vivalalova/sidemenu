854921c7c2db3aa6cf30a9990a23b84c81cc103e
vivalalova <vivalalova0@gmail.com>
2021-09-11 01:11:37 +0800

    Initial Commit

---
+++ sidemenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sidemenu
+//
+//  Created by Lova on 2021/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sidemenu/sidemenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  sidemenuApp.swift
+//  sidemenu
+//
+//  Created by Lova on 2021/9/11.
+//
+
+import SwiftUI
+
+@main
+struct sidemenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
